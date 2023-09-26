6f464136b69ce79b8bb4e228b0af40229f90116e
Reza Banankhah <banankha@usc.edu>
2023-09-26 13:07:59 -0700

    Installed ui and icon libraries
    Installed DaisyUI and unplugin icons / iconify icons
    Added custom colors to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,26 @@
 export default {
   content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      'white': '#ffffff',
+      'bone': '#f7f7f7',
+      'moonlight': '#FFF8DE',
+      'midnight': {
+        950: '#070715',
+        900: '#0b0a20',
+        800: '#0f0e2a',
+        700: '#131135',
+        600: '#161540',
+        500: '#1a184a',
+        400: '#1e1c55',
+        300: '#221f60',
+        200: '#26236b',
+      },
+    },
     extend: {},
   },
-  plugins: [],
+  plugins: [require("daisyui")],
 }
 
